20c5d8099e57df328bd0f13e1e33cfa1c36b300f
sourav <ajaypratap.singh@geeky.dev>
2024-08-22 15:30:37 +0530

    sign up and sign in and middleware setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,28 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { sign } from "hono/jwt";
+import { sign, verify } from "hono/jwt";
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
     JWT_SECRET: string;
   };
 }>();
+// MiddleWare
+app.use("/api/v1/blog/*", async (c, next) => {
+  const header = c.req.header("authorization" || "");
+  const token = header?.split(" ")[1];
 
+  const response = await verify(token, c.env.JWT_SECRET);
+  if (response.id) {
+    next();
+  } else {
+    c.status(403);
+    return c.json({ error: "unauthorized" });
+  }
+});
+
+// Routes
 app.get("/", (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
@@ -19,7 +33,6 @@ app.post("/api/v1/signup", async (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
-
   const body = await c.req.json();
   try {
     const user = await prisma.user.create({
@@ -29,10 +42,10 @@ app.post("/api/v1/signup", async (c) => {
       },
     });
     const token = await sign({ id: user.id }, c.env.JWT_SECRET);
-    console.log("token", token);
     return c.json({ token });
   } catch (error) {
     c.status(403);
+    console.log(error);
     return c.json({ error: "error while signing up" });
   }
 });
@@ -46,6 +59,7 @@ app.post("/api/v1/signin", async (c) => {
   const user = await prisma.user.findUnique({
     where: {
       email: body.email,
+      password: body.password,
     },
   });
 
